7fb5f1dbba71046a55039e79f8d56c76c084548c
sabrina34352 <s.babakulova@student.inha.uz>
2022-04-09 21:47:38 +0500

    added secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth";
-import GithubProvider from 'next-auth/providers/github';
+import GithubProvider from "next-auth/providers/github";
 export default NextAuth({
-providers:[
+  providers: [
     GithubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    })
-]
-})
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.SECRET,
+});
