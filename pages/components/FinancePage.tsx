e79b880273fd0a66be35105dfd7b43b319f7f0e1
sabrina34352 <s.babakulova@student.inha.uz>
2022-04-09 20:43:48 +0500

    authorization, pages, links

--- pages/components/FinancePage.tsx
+++ pages/components/FinancePage.tsx
@@ -1,7 +1,28 @@
 
+import { Box, Text, Flex, Divider } from "@chakra-ui/react"
+import Menu from './Menu'
+import styles from '../../styles/Home.module.css'
+
+import SignIn from './SignIn'
+import { useSession, signOut } from 'next-auth/react'
+
 function FinancePage() {
+
     return (
-        <div>FinancePage</div>
+        <>
+            <Box className={styles.container}>
+                <Menu />
+                <Flex justify="space-around">
+                    <Text>
+                        income
+                    </Text>
+                    <Text>
+                        outcome
+                    </Text>
+
+                </Flex>
+            </Box>
+        </>
     )
 }
 
--- pages/components/InhaPage.tsx
+++ pages/components/InhaPage.tsx
@@ -1,23 +1,36 @@
 // import React from 'react'
 import { Box, Text, Flex } from "@chakra-ui/react"
+import Menu from './Menu'
+import styles from '../../styles/Home.module.css'
 
+import SignIn from './SignIn'
+import { useSession, signOut } from 'next-auth/react'
 
 function InhaPage() {
+    const { data: session } = useSession();
+    if (!session) {
+        // return <>
+        //     <SignIn />
+        // </>
+        // Router.push('/');
+    }
     return (
-        // <div>InhaPage</div>
-        <Box>
-            <Flex>
+        <>
+            <Box className={styles.container}>
+                <Menu />
+                <Flex justify="space-around">
 
-                <Text>
-                    Deadlines
-                </Text>
+                    <Text>
+                        Deadlines
+                    </Text>
 
-                <Text>
-                    New stuff
-                </Text>
+                    <Text>
+                        New stuff
+                    </Text>
 
-            </Flex>
-        </Box>
+                </Flex>
+            </Box>
+        </>
     )
 }
 
