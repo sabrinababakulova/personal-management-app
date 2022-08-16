7bb4e4e314c54778caec8e683816e12b6d4df56b
sabrina34352 <s.babakulova@student.inha.uz>
2022-08-16 17:20:23 +0500

    huge change to ui

---
+++ src/components/Links/index.tsx
@@ -0,0 +1,22 @@
+import Telegram from "../../assets/Telegram.svg";
+import Instagram from "../../assets/Instagram.svg";
+import LinkedIn from "../../assets/LinkedIn.svg";
+import Github from "../../assets/Github.svg";
+export const links = [
+  {
+    href: "https://t.me/get_rektttt",
+    icon: <Telegram />,
+  },
+  {
+    href: "https://www.instagram.com/sabrina_smthelse/",
+    icon: <Instagram />,
+  },
+  {
+    href: "https://www.linkedin.com/in/sabrina-babakulova-a8a8a8a8/",
+    icon: <LinkedIn />,
+  },
+  {
+    href: "https://www.github.com/sabrina34352",
+    icon: <Github />,
+  },
+];
